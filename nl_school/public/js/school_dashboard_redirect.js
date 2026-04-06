10e4cf765bb4ecfb90dd4624b567955afabdfc57
Habert Kungu <habert231@gmail.com>
2026-04-06 19:30:05 +0300

    Tighten teacher redirect - faster redirect on page load

--- nl_school/public/js/school_dashboard_redirect.js
+++ nl_school/public/js/school_dashboard_redirect.js
@@ -1,12 +1,22 @@
+// Redirect teachers to teacher-dashboard immediately
 frappe.router.on("change", function() {
     if (frappe.router.current_route[0] === "school-dashboard") {
-        frappe.call({
-            method: "nl_school.junior_school_customization.page.school_dashboard.school_dashboard.check_role",
-            callback: function(r) {
-                if (r.message === "teacher") {
-                    window.location.href = "/desk/teacher-dashboard";
-                }
-            }
-        });
+        // Check role immediately via localStorage or session
+        var user_roles = frappe.boot ? frappe.boot.user.roles : [];
+        
+        // If teacher (has Instructor but not Education Manager)
+        if (user_roles.indexOf("Instructor") !== -1 && user_roles.indexOf("Education Manager") === -1) {
+            window.location.href = "/desk/teacher-dashboard";
+        }
+    }
+});
+
+// Also check on page load in case routing already happened
+frappe.ready(function() {
+    if (window.location.pathname.indexOf("school-dashboard") !== -1) {
+        var user_roles = frappe.boot ? frappe.boot.user.roles : [];
+        if (user_roles.indexOf("Instructor") !== -1 && user_roles.indexOf("Education Manager") === -1) {
+            window.location.href = "/desk/teacher-dashboard";
+        }
     }
 });
